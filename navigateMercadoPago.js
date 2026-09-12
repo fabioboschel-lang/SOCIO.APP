b986488fbd9246f0198eb41efa39a403e23ac73f
fabioboschel-lang <fabioboschel@gmail.com>
2026-09-12 01:34:00 -0300

    Crear navigateMercadoPago.js

---
+++ navigateMercadoPago.js
@@ -0,0 +1,30 @@
+
+
+
+export function NavigateMercadoPago() {
+
+  const mercadopago =
+    document.createElement("div");
+
+
+  mercadopago.className =
+    "navigate-ux-item";
+
+
+  mercadopago.textContent =
+    "Mercado Pago";
+
+
+  mercadopago.addEventListener(
+    "click",
+    () => {
+
+      navigate("mercadopago");
+
+    }
+  );
+
+
+  return mercadopago;
+
+}
