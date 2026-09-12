616db96430ca7cc1ff3764e749a087e911fa3612
fabioboschel-lang <fabioboschel@gmail.com>
2026-09-12 02:36:38 -0300

    Update home.js

--- home.js
+++ home.js
@@ -15,7 +15,7 @@ export function Home(app) {
       <section class="event-link-container">
 
         <h1 class="event-link-title">
-          Enlace lmd
+          Enlace lm
         </h1>
 
         <p
