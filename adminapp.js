70270cc3cf3a5897212e139edeeadbe34266fc9d
fabioboschel-lang <fabioboschel@gmail.com>
2026-09-12 00:55:06 -0300

    Actualizar adminapp.js

--- adminapp.js
+++ adminapp.js
@@ -1,51 +1,15 @@
-import { supabase, authInicializada } from "./supabase.js";
-import { navigate } from "./navigate.js";
+import {
+  supabase,
+  authInicializada
+} from "./supabase.js";
 
+import {
+  navigate
+} from "./navigate.js";
 
-/* =========================
-   OBTENER USUARIO LOCAL
-   ========================= */
-
-function obtenerUsuarioLocal() {
-
-  const key =
-    "sb-qexgbswdbwlpydolpcll-auth-token";
-
-
-  const storedSession =
-    localStorage.getItem(key);
-
-
-  if (!storedSession) {
-
-    return null;
-
-  }
-
-
-  try {
-
-    const session =
-      JSON.parse(storedSession);
-
-
-    return (
-      session?.user?.id ??
-      null
-    );
-
-  } catch (error) {
-
-    console.error(
-      "Sesión local inválida:",
-      error
-    );
-
-    return null;
-
-  }
-
-}
+import {
+  obtenerUsuarioLocal
+} from "./usuarioID.js";
 
 
 /* =========================
